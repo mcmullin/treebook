6e9d260ccb30cee020e5ea2e78b87a3baa725868
Robert McMullin <robert.mcmullin@gmail.com>
2013-07-25 21:02:38 -0400

    Updated frontend for the new authentication system
    Added simple_form
    Updated several forms to Bootstrap/simple_form
    Added login/logout/register/profile links to our nav
    Added a collection select to our status form

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,10 @@ class User < ActiveRecord::Base
   # attr_accessible :title, :body
 	attr_accessible :first_name, :last_name, :profile_name, 
 									:email, :password, :password_confirmation, :remember_me
+									
+	has_many :statuses
+									
+	def full_name
+		first_name + " " + last_name
+	end
 end
