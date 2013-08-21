6914b829be835c3b34462a55f30b765ee3e5474f
Robert McMullin <robert.mcmullin@gmail.com>
2013-08-20 23:11:33 -0400

    improve gravatar_url method

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,9 +19,7 @@ class User < ActiveRecord::Base
 	end
 	
 	def gravatar_url
-		stripped_email = email.strip
-		downcased_email = stripped_email.downcase
-		hash = Digest::MD5.hexdigest(downcased_email)
-		"http://gravatar.com/avatar/#{hash}"
+    hash = Digest::MD5.hexdigest(email.downcase.strip)
+    "http://gravatar.com/avatar/#{hash}"
 	end
 end
