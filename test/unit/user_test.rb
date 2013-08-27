6586fecf324385f15c847eebfe3178a417af0b7f
Robert McMullin <robert.mcmullin@gmail.com>
2013-08-26 23:17:22 -0400

    massive commit: request, accept, delete friendships

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -52,8 +52,12 @@ class UserTest < ActiveSupport::TestCase
 	end
 	
 	test "that creating friendships on a user works" do
-		users(:jason).friends << users(:mike)
-		users(:jason).friends.reload
-		assert users(:jason).friends.include?(users(:mike))
+		users(:jason).pending_friends << users(:mike)
+		users(:jason).pending_friends.reload
+		assert users(:jason).pending_friends.include?(users(:mike))
+	end
+	
+	test "that calling to_param on a user returns the profile name" do
+		assert_equal "jasonseifer", users(:jason).to_param
 	end
 end
